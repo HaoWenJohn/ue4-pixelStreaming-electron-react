521ca888cf702892857ac8872880148edb1f16ae
ZhengHaoWen <705680835@qq.com>
2021-08-05 11:47:41 +0800

    fix readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+// @ts-ignore
 import App from './main_page/App';
 import './App.global.css'
 render(<App />, document.getElementById('root'));
